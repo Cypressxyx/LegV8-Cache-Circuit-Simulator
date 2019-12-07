e31b44bd16456fba15989b9fb70375fd2303b3d7
jorgexyx <bautisjo@sonoma.edu>
2019-12-06 16:20:35 -0800

    Removed unsued code

--- cache.py
+++ cache.py
@@ -1,21 +1,12 @@
-from memory import Memory
-
 """
 Test the cache using example
 From Class
 There should only be a few misses 8 / 24? 
 """
+from memory import Memory
+
 mem = Memory()
 for i in range(2):
 	for j in range(10):
 		mem.load(j)
-
 print(mem.get_misses())
-"""
-def test_cache():
-	for i in range(2):
-		for j in range(23):
-			print(j)
-			#check_hit_or_miss(j)
-
-"""
