4ab85a9d2ea3cf6603b58144009b1d36bd1ea221
jorgexyx <bautisjo@sonoma.edu>
2019-12-07 12:48:14 -0800

    Generalized bit getting

--- MemoryModule/memory_tools.py
+++ MemoryModule/memory_tools.py
@@ -1,26 +1,20 @@
 # return first 3 bits and last 2 bits
 def parse_memory_location_five_bits(value):
-	return get_index(value), get_key(value)
+	tag   = get_bits(value, 2, 2)
+	index = get_bits(value, 3, 0)
+	return index, tag
 
-# return first 3 bits and last 2 bits
+# returna index tag and offset in a value 
 def parse_memory_location_8_bits(value):
-	return get_tag_last_bits(value), get_index_bits(value), get_offset(value)
+	tag    = get_bits(value, 2, 6)
+	index  = get_bits(value, 3, 3)
+	offset = get_bits(value, 3, 0)
+	return tag, index, offset
 
-# return first 3 bits using the value 7 000....1110000
-def get_index(value):
-	return value & 7
-
-# return last 2 bits using the value 24 11000
-def get_key(value):
-	return (value & 24) >> 3
-
-# return last 2 bits using the value 56 11000000
-def get_tag_last_bits(value):
-	return (value & 192) >> 66
-
-def get_index_bits(value):
-	return (value & 56) >> 3
-
-def get_offset(value):
-	return get_index(value)
-
+# return the nth bits 
+def get_bits(value, num_bits, starting_position):
+	mask = 1
+	for i in range(1, num_bits):	
+		mask = mask << 1 | 1
+	mask = mask << starting_position
+	return (value & mask) // (2**starting_position)
